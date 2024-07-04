25c0ca24d059f8a230ac41cfc6db4fa3f2607a8c
Zawadi <zawadilois@gmail.com>
2024-07-04 17:45:12 +0300

    update dashboard page

--- pages/02_📈_Dashboard.py
+++ pages/02_📈_Dashboard.py
@@ -10,7 +10,7 @@ st.set_page_config(
 st.title('Dashboard 📈')
 st.sidebar.success("Select a page")
 
-df = pd.read_csv('./Data/training_data.csv')
+df = pd.read_csv('./data/training_data.csv')
 
 # Filters on sidebar
 st.sidebar.subheader('Filters')
