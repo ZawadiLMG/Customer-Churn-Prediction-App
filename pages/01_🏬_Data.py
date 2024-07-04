ded9ba00e46d380d9e6eb295338720b3a3b7eb68
Zawadi <zawadilois@gmail.com>
2024-07-04 19:08:09 +0300

    update

--- pages/01_🏬_Data.py
+++ pages/01_🏬_Data.py
@@ -10,38 +10,9 @@ st.set_page_config(
 
 st.title('Telcom Customer Churn Database 🏬')
 st.sidebar.success("select a page above")
-
-## create a connection to a database
-@st.cache_resource(show_spinner="Connecting to database...")
-def init_connection():
-    connection_string = (
-        "DRIVER={SQL Server};"
-        "SERVER=" + st.secrets['server'] + ";"
-        "DATABASE=" + st.secrets['database'] + ";"
-        "UID=" + st.secrets['username'] + ";"
-        "PWD=" + st.secrets['password']
-    )
-    return pyodbc.connect(connection_string)
- 
-conn = init_connection()
- 
-@st.cache_data(show_spinner="Running query...")
-def running_query(query):
-    with conn.cursor() as cursor:
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        columns = [column[0] for column in cursor.description]
-        df = pd.DataFrame.from_records(rows, columns=columns)
-    return df
- 
- 
-def get_all_columns():
-    sql_query = "SELECT * FROM dbo.LP2_Telco_churn_first_3000"
-    df = running_query(sql_query)
-    return df
  
 # Load data
-df = get_all_columns()
+df = pd.read_csv('.data/training_data.csv')
  
 # Identify categorical and numerical columns
 categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
